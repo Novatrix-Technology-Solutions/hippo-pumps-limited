875e4587f240468d5d751b4fd68444d3be751783
mthunzi-musumadi <musumadimthunzi@gmail.com>
2025-04-22 05:35:02 +0200

    minor changes to configs

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,11 @@ import tailwindcss from 'tailwindcss';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.tsx'],
+            input: [
+                'resources/css/app.css',
+                'resources/js/app.tsx',
+                'resources/js/pages/**/*.tsx'
+            ],
             // ssr: 'resources/js/ssr.tsx',
             refresh: true,
         }),
