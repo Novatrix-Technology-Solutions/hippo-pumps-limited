334a51542bf7173ec919ec00c43555a61c267df2
mthunzi-musumadi <musumadimthunzi@gmail.com>
2025-04-23 03:28:07 +0200

    Update AdminLayout.tsx

--- resources/js/layouts/AdminLayout.tsx
+++ resources/js/layouts/AdminLayout.tsx
@@ -107,6 +107,7 @@ const AdminLayout = ({ children, title, user }: PropsWithChildren<Props>) => {
                                             className="text-gray-500 hover:text-gray-700"
                                         >
                                             <LogOut className="w-5 h-5" />
+                                            Log Out
                                         </Link>
                                     </div>
                                 </div>
