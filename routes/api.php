d9a26041f37ab74eb33d41d106850011da41cde5
mthunzi-musumadi <musumadimthunzi@gmail.com>
2025-04-29 11:24:33 +0200

    create CorsMiddleware.php

--- routes/api.php
+++ routes/api.php
@@ -19,4 +19,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/pumps', [PumpSolutionController::class, 'apiIndex']);
+Route::get('/pumps', [PumpSolutionController::class, 'apiIndex'])->middleware(\App\Http\Middleware\CorsMiddleware::class);
