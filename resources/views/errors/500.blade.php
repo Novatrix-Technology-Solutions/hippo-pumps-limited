6a85a6481ebcb07914e8762896a84f238c0352bb
Mthunzi Musumadi <musumadimthunzi@gmail.com>
2025-05-07 19:54:45 +0200

    Create 500.blade.php

---
+++ resources/views/errors/500.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Server Error</title>
+</head>
+<body>
+    <h1>502 - Bad Gateway</h1>
+    <p>Oops! Something went wrong on the server.</p>
+    <p>Please try again later.</p>
+</body>
+</html>
